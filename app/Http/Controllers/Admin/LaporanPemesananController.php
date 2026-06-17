1061219121150117f837246809c99fa94dae5ffb
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2026-06-17 16:30:57 +0800

    fix: move closure route to invokable controller to enable route:cache
    php artisan route:cache gagal karena Route::get('pemesanan/laporan', function() {...})
    tidak bisa di-serialize. Akibatnya route livewire.preview-file tidak terdaftar
    dan preview upload 404.
    
    - Create App\Http\Controllers\Admin\LaporanPemesananController (invokable)
    - Replace closure with class reference in routes/web.php

---
+++ app/Http/Controllers/Admin/LaporanPemesananController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LaporanPemesananController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $bulan = $request->input('bulan', date('m'));
+        $tahun = $request->input('tahun', date('Y'));
+
+        $pemesanans = \App\Models\Pemesanan::with(['pelanggan', 'kendaraanUnit.kendaraan', 'promo'])
+            ->whereMonth('created_at', $bulan)
+            ->whereYear('created_at', $tahun)
+            ->whereIn('status_pemesanan', ['selesai', 'disetujui'])
+            ->get();
+
+        $totalPendapatan = $pemesanans->sum('total_harga');
+        $totalDenda = $pemesanans->sum('denda');
+
+        return view('pages.admin.pemesanan.laporan', compact('pemesanans', 'bulan', 'tahun', 'totalPendapatan', 'totalDenda'));
+    }
+}
